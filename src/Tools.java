3187068a859b2f2fbe63a1a22b8c93dd345c3c6d
shenCode <yangshen@gatech.edu>
2014-09-11 18:09:32 -0400

    Updated Dice

--- src/Tools.java
+++ src/Tools.java
@@ -2,6 +2,10 @@ import java.util.Random;
 
 public class Tools {
 
+    /**
+     * @param num The number of dice to be rolled.
+     * @return the value from the dice/s.
+     */
     public static int rollDice(int num) {
         int total = 0;
         Random rand = new Random();
