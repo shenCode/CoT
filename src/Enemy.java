17e6868e0addab4d5aa14258336d921e774bc874
Bo Chen <bochentheone@gmail.com>
2014-09-13 20:10:00 -0400

    enemy class update

--- src/Enemy.java
+++ src/Enemy.java
@@ -3,10 +3,24 @@ public class Enemy {
     private String name;
     private int skill;
     private int stamina;
+    private boolean alive;
 
     public Enemy(String n, int sk, int st) {
         name = n;
         skill = sk;
         stamina = st;
+        alive = true;
     }
+    
+    public void setDead(){
+    	alive = false;
+    	stamina = 0;
+    }
+    
+    public void setStamina(int change){
+    	stamina += change;
+    }
+    
+    
+    
 }
